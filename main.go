e2992c4086a1b01a29c92cb1427b759b188a0b96
Hai (James) <mai.son.hai@sun-asterisk.com>
2026-05-28 11:23:13 +0700

    Graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -38,8 +42,32 @@ func getAccounts(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/accounts", getAccounts)
-	log.Fatal(http.ListenAndServe(":8080", nil)) // With nil, using DefaultServeMux
+	 
 	// Config this server Manually
+	srv := http.Server{
+		Addr: 8080,
+		Handler: mux,
+		ReadTimeout: 60 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout: 120 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe()) 
 
 	// Graceful Shutdown
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error!!", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit  // We're waiting
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Shutdown Failed: ", err)
+	}
 }
