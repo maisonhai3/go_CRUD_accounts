b284f38cc9c8829cc6b16a5b044052c1416ac949
James.Mai <maisonhai3@gmail.com>
2026-06-24 13:27:53 +0700

    Update streaming_txn.go

--- streaming_exp/streaming_txn.go
+++ streaming_exp/streaming_txn.go
@@ -1,1 +1,44 @@
 package streaming_exp
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func StreamingTxn(api string) {
+	res, err := http.Get(api)
+	if err != nil {
+		log.Fatal() // enhance later
+		return
+	}
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+
+	// Opening the stream
+	if _, err := decoder.Token(); err != nil {
+		log.Fatal()
+		return
+	}
+
+	for decoder.More() {
+		var txn Transaction
+
+		if err := decoder.Decode(&txn); err != nil {
+			log.Fatal()
+		}
+
+		fmt.Printf("ID: %s", txn.ID)
+		fmt.Printf("Amount: %f", txn.Amount)
+		fmt.Printf("Status: %s", txn.Status)
+
+	}
+
+	// Closing the stream
+	if _, err := decoder.Token(); err != nil {
+		log.Fatal()
+		return
+	}
+}
